6437003529ed605d968689f419f10b40349f7bad
Udhaya <trn.udhaya@gmail.com>
2022-01-05 12:34:46 -0800

    Split query url

--- simple-moduledemo.js
+++ simple-moduledemo.js
@@ -2,9 +2,15 @@
 var http = require('http');
 var dt = require('./simple-module');
 
+//Split query url
+var url = require('url');
+
 http.createServer(function (req,res) {
     res.writeHead(200, {'Content-Type':'text/html'});
     res.write("The current date and time:"+dt.myDateTime());
-    res.write(req.url);
-    res.end();
+    //res.write(req.url);
+
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.end(txt);
 }).listen(8080);
