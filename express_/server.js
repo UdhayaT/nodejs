010def1ff47d49af1a3b15663730bfdbf9fda7c5
Udhaya <trn.udhaya@gmail.com>
2022-01-09 13:23:55 -0800

    added backend messages

--- express_/server.js
+++ express_/server.js
@@ -3,6 +3,16 @@ var app = express()
 
 
 app.use(express.static(__dirname))
+
+var messages = [
+    {name:"TheOne", message:"We're watching you!"},
+    {name:"Black Cat", message:"Deja Vu. is it?"}
+]
+
+app.get('/message', (req, res) => {
+    res.send(messages)
+})
+
 var server = app.listen(3000, () => {
     console.log("server is listening at port:"+server.address().port)
 })
