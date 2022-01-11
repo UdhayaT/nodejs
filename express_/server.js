dae8207d3a067f86723fd14c86b3e3852940eed5
uthiagar <uthiagar@usc.edu>
2022-01-11 15:30:27 -0800

    post message

--- express_/server.js
+++ express_/server.js
@@ -1,8 +1,13 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 
 var messages = [
     {name:"TheOne", message:"We're watching you!"},
@@ -13,6 +18,13 @@ app.get('/message', (req, res) => {
     res.send(messages)
 })
 
-var server = app.listen(3000, () => {
+app.post('/message', (req, res) => {
+    //console.log(req.body)
+    messages.push(req.body)
+    console.log(messages)
+    res.sendStatus(200)
+})
+
+var server = http.listen(3000, () => {
     console.log("server is listening at port:"+server.address().port)
 })
