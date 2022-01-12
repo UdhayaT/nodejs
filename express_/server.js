7795d0fbfd0f95aff3407900d8ed2517754d62bc
uthiagar <uthiagar@usc.edu>
2022-01-12 08:53:02 -0800

    mongodb for message save

--- express_/server.js
+++ express_/server.js
@@ -3,30 +3,46 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+//Mongoose: For connecting with MongoDB
 var mongoose = require('mongoose')
+const { sendStatus } = require('express/lib/response')
 
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
-var messages = [
-    {name:"TheOne", message:"We're watching you!"},
-    {name:"Black Cat", message:"Deja Vu. is it?"}
-]
+// var messages = [
+//     {name:"TheOne", message:"We're watching you!"},
+//     {name:"Black Cat", message:"Deja Vu. is it?"}
+// ]
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
 
 var dbUrl = "mongodb://localhost:27017"
 
 app.get('/message', (req, res) => {
-    res.send(messages)
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
 })
 
 app.post('/message', (req, res) => {
     //console.log(req.body)
-    messages.push(req.body)
-    console.log(messages)
-    io.emit('message',req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
+    message.save((err) => {
+        if(err) 
+            sendStatus(500)
+
+        //messages.push(req.body)
+        //console.log(messages)
+        io.emit('message',req.body)
+        res.sendStatus(200)
+    })
+    
 })
 
 io.on('connection',(socket) => {
